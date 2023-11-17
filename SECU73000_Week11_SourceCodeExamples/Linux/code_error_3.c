99d95701dd0c1f479d969f2a5da14a3728d010da
Artem Maksymov <42908929+armaksymov@users.noreply.github.com>
2023-11-17 16:23:54 -0500

    Added all the needed files

---
+++ SECU73000_Week11_SourceCodeExamples/Linux/code_error_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Please enter the a number: ");
+    scanf("%i", &num);
+    printf("You entered %d\n", num);
+    return 0;
+}
---
+++ SECU73000_Week11_SourceCodeExamples/Windows/four/Source.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void process_string(const char *src)
+{
+	char dest[32];
+
+	for (int i = 0; src[i] && (i <= sizeof(dest)); i++)
+		dest[i] = src[i];
+	printf("%s\n", dest);
+}
+
+int main()
+{
+	process_string("The quick brown fox jumped over the lazy dog");
+}
